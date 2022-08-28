849a7e8fc45d813e81ae5fe8c2fe906c08528c53
Jason Au <59789630+jasonau2003@users.noreply.github.com>
2022-08-29 00:01:47 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,26 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '490da54720c0441ea2b1d64066546fa4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: "ea7e8639-04a9-4b2d-87e9-4bdbebb0a64d",
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Jason’s Blog',
+  domain: 'blog.jsa.hk',
+  author: 'Jason Au Inc. All rights reserved.',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // description: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
-  // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  // twitter: '',
+  github: 'jasonau2003',
+  // linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -29,13 +27,16 @@ export default siteConfig({
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
-  // whether or not to enable support for LQIP preview images (optional)
-  isPreviewImageSupportEnabled: true,
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
 
-  // whether or not redis is enabled for caching generated preview images (optional)
-  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
-  // environment variables. see the readme for more info
-  isRedisEnabled: false,
+  // Utteranc.es comments via GitHub issue comments (optional)
+  //utterancesGitHubRepo: "jasonau2003/blog-jsa-hk-comment",
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -45,7 +46,13 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
+  pageUrlOverrides:   {
+    '/notion-as-blog': '2ac48272b7644cb0b0f8d19a104ddf05',
+    '/refers': '68892ddbd87341a5940b08fe9f3d079f',
+    '/digital-noting-tools-i-use': '7a26482a6a774a8dbd4872d9023b3957',
+    '/about': '0da3484664b44dcaa555509e3eb927f1',
+    '/archive': '299f3286fd3643ddbf6f4489b4813e81'
+},
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
